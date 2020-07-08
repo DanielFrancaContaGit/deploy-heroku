305e71271536f612b0e38e5e4635c0a5b141faad
DanielFrancaContaGit <daniel.pe.15y15@gmail.com>
2020-07-08 13:33:37 -0300

    fist commit

---
+++ public/precache-manifest.caaa28155bcda1eea2e7df6b5629e8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc714f9a488a7d88968f151e158e2c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf6fdf6b149749c837d",
+    "url": "/static/css/main.046326dd.chunk.css"
+  },
+  {
+    "revision": "7595c9fc0b4a57b29d3a",
+    "url": "/static/js/2.9b3c3b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b3c3b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf6fdf6b149749c837d",
+    "url": "/static/js/main.deb70aae.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
